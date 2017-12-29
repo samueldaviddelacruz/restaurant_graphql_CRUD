1faf7fcddfd4616188f5b7348c3e693b299c28f8
Samuel David De La Cruz <alphaelena@gmail.com>
2017-12-28 22:06:13 -0400

    -Created Delete mutation for Categories -Created Create,Update,Delete Mutations for Dishes

--- restaurant_graphql_api/settings.py
+++ restaurant_graphql_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'restaurantapi',
+    'corsheaders',
 
 ]
 
@@ -47,14 +48,27 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
 
 ROOT_URLCONF = 'restaurant_graphql_api.urls'
 
